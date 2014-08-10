6cebe752d2233eb6ba919779eaaa187bf0e34be3
doraemonext <doraemonext@gmail.com>
2014-08-10 11:49:44 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'wechat-sdk',
-    version = '0.1',
+    version = '0.1.1',
     keywords = ('wechat', 'sdk'),
     description = 'A Python SDK for WeChat.',
     license = 'BSD License',
