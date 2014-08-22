9b1704ed68f4acb8bb6286bd4804fd54e5600ea8
doraemonext <doraemonext@gmail.com>
2014-08-22 10:16:05 +0800

    Add: 更新获取消息列表内容

--- docs/ext.rst
+++ docs/ext.rst
@@ -1,4 +1,79 @@
 微信非官方接口操作 WechatExt
 ============================
 
-请耐心等待文档完善，会尽快完成。
+.. py:class:: wechat_sdk.ext.WechatExt(username, password [, token=None, cookies=None, ifencodepwd=False])
+
+    微信扩展功能类
+
+    :param str username: 你的微信公众平台账户用户名
+    :param str password: 你的微信公众平台账户密码
+    :param str token: 直接导入的 ``token`` 值, 该值需要在上一次该类实例化之后手动进行缓存并在此传入, 如果不传入, 将会在实例化的时候自动获取
+    :param str cookies: 直接导入的 ``cookies`` 值, 该值需要在上一次该类实例化之后手动进行缓存并在此传入, 如果不传入, 将会在实例化的时候自动获取
+    :param str ifencodepwd: 密码是否已经经过编码, 如果密码已经经过加密, 此处为 ``True`` , 如果传入的密码为明文, 此处为 ``False``
+
+    .. py:method:: login()
+
+        登录微信公众平台
+
+        注意在实例化 ``WechatExt`` 的时候，如果没有传入 ``token`` 及 ``cookies`` ，将会自动调用该方法，无需手动调用
+
+        当且仅当捕获到 ``NeedLoginError`` 需要登录异常时才需要调用此方法进行登录重试
+
+        :raises: LoginError 登录出错异常，异常内容为微信服务器响应的内容，可作为日志记录下来
+
+    .. py:method:: get_message_list()
+
+        获取消息列表
+
+        返回JSON示例 ::
+
+            {
+                "msg_item": [
+                    {
+                        "id": 206439583,
+                        "type": 1,
+                        "fakeid": "844735403",
+                        "nick_name": "Doraemonext",
+                        "date_time": 1408671892,
+                        "content": "测试消息",
+                        "source": "",
+                        "msg_status": 4,
+                        "has_reply": 0,
+                        "refuse_reason": "",
+                        "multi_item": [ ],
+                        "to_uin": 2391068708,
+                        "send_stat": {
+                            "total": 0,
+                            "succ": 0,
+                            "fail": 0
+                        }
+                    },
+                    {
+                        "id": 206439579,
+                        "type": 1,
+                        "fakeid": "844735403",
+                        "nick_name": "Doraemonext",
+                        "date_time": 1408671889,
+                        "content": "wechat-python-sdk",
+                        "source": "",
+                        "msg_status": 4,
+                        "has_reply": 0,
+                        "refuse_reason": "",
+                        "multi_item": [ ],
+                        "to_uin": 2391068708,
+                        "send_stat": {
+                            "total": 0,
+                            "succ": 0,
+                            "fail": 0
+                        }
+                    }
+                ]
+            }
+
+        :param integer lastid: 传入最后的消息 id 编号, 为 0 则从最新一条起倒序获取
+        :param integer offset: lastid 起算第一条的偏移量
+        :param integer count: 获取数目
+        :param integer day: 最近几天消息 (0: 今天, 1: 昨天, 2: 前天, 3: 更早, 7: 全部), 这里的全部仅有5天
+        :param boolean star: 是否只获取星标消息
+        :return: 返回的 JSON 数据
+        :raises: NeedLoginError 需要再次尝试登录, 异常内容为服务器返回的错误数据
--- wechat_sdk/ext.py
+++ wechat_sdk/ext.py
@@ -3,6 +3,7 @@
 import hashlib
 import re
 import requests
+import json
 
 from .exceptions import UnOfficialAPIError, NeedLoginError, LoginError
 
@@ -13,40 +14,42 @@ class WechatExt(object):
 
     通过模拟登陆的方式实现更多的高级功能, 请注意使用本类有风险, 请自行承担
     """
-    def __init__(self, account, pwd, token=None, cookies=None, ifencodepwd=False):
+    def __init__(self, username, password, token=None, cookies=None, ifencodepwd=False):
         """
-        :param account: 账户名称(用户名)
-        :param pwd: 密码
-        :param token: token
-        :param cookies: cookie
-        :param ifencodepwd: 密码是否已加密
+        :param username: 你的微信公众平台账户用户名
+        :param password: 你的微信公众平台账户密码
+        :param token: 直接导入的 ``token`` 值, 该值需要在上一次该类实例化之后手动进行缓存并在此传入, 如果不传入, 将会在实例化的时候自动获取
+        :param cookies: 直接导入的 ``cookies`` 值, 该值需要在上一次该类实例化之后手动进行缓存并在此传入, 如果不传入, 将会在实例化的时候自动获取
+        :param ifencodepwd: 密码是否已经经过编码, 如果密码已经经过加密, 此处为 ``True`` , 如果传入的密码为明文, 此处为 ``False``
         :return:
         """
-        self.account = account
+        self.__username = username
         if ifencodepwd:
-            self.pwd = pwd
+            self.__password = password
         else:
-            self.pwd = hashlib.md5(pwd).hexdigest()
-        self.wx_cookies = cookies
-        self.lastmsgid = 0
-        self.token = token
+            self.__password = hashlib.md5(password).hexdigest()
+        self.__cookies = cookies
+        self.__lastmsgid = 0
+        self.__token = token
 
-        if not self.token or not self.wx_cookies:
-            self.token = ''
-            self.wx_cookies = ''
+        if not self.__token or not self.__cookies:
+            self.__token = ''
+            self.__cookies = ''
             self.login()
 
     def login(self):
         """
-        模拟登陆微信公众平台
-        :raises LoginError: 登录出错
+        登录微信公众平台
+        注意在实例化 ``WechatExt`` 的时候，如果没有传入 ``token`` 及 ``cookies`` ，将会自动调用该方法，无需手动调用
+        当且仅当捕获到 ``NeedLoginError`` 需要登录异常时才需要调用此方法进行登录重试
+        :raises LoginError: 登录出错异常，异常内容为微信服务器响应的内容，可作为日志记录下来
         """
         url = 'https://mp.weixin.qq.com/cgi-bin/login?lang=zh_CN'
         payload = {
-            'username': self.account,
+            'username': self.__username,
             'imgcode': '',
             'f': 'json',
-            'pwd': self.pwd,
+            'pwd': self.__password,
         }
         headers = {
             'x-requested-with': 'XMLHttpRequest',
@@ -56,41 +59,97 @@ class WechatExt(object):
 
         s = re.search(r'token=(\d+)', r.text)
         if not s:
-            raise LoginError()
-        self.token = int(s.group(1))
+            raise LoginError(r.text)
+        self.__token = int(s.group(1))
 
-        self.wx_cookies = ''
+        self.__cookies = ''
         for cookie in r.cookies:
-            self.wx_cookies += cookie.name + '=' + cookie.value + ';'
+            self.__cookies += cookie.name + '=' + cookie.value + ';'
 
-    def get_message_list(self):
+    def get_message_list(self, lastid=0, offset=0, count=20, day=7, star=False):
         """
         获取消息列表
+
+        返回JSON示例::
+            {
+                "msg_item": [
+                    {
+                        "id": 206439583,
+                        "type": 1,
+                        "fakeid": "844735403",
+                        "nick_name": "Doraemonext",
+                        "date_time": 1408671892,
+                        "content": "测试消息",
+                        "source": "",
+                        "msg_status": 4,
+                        "has_reply": 0,
+                        "refuse_reason": "",
+                        "multi_item": [ ],
+                        "to_uin": 2391068708,
+                        "send_stat": {
+                            "total": 0,
+                            "succ": 0,
+                            "fail": 0
+                        }
+                    },
+                    {
+                        "id": 206439579,
+                        "type": 1,
+                        "fakeid": "844735403",
+                        "nick_name": "Doraemonext",
+                        "date_time": 1408671889,
+                        "content": "wechat-python-sdk",
+                        "source": "",
+                        "msg_status": 4,
+                        "has_reply": 0,
+                        "refuse_reason": "",
+                        "multi_item": [ ],
+                        "to_uin": 2391068708,
+                        "send_stat": {
+                            "total": 0,
+                            "succ": 0,
+                            "fail": 0
+                        }
+                    }
+                ]
+            }
+
+        :param lastid: 传入最后的消息 id 编号, 为 0 则从最新一条起倒序获取
+        :param offset: lastid 起算第一条的偏移量
+        :param count: 获取数目
+        :param day: 最近几天消息 (0: 今天, 1: 昨天, 2: 前天, 3: 更早, 7: 全部), 这里的全部仅有5天
+        :param star: 是否只获取星标消息
         :return: 返回的 JSON 数据
-        :raises NeedLoginError: 需要再次尝试登录
+        :raises NeedLoginError: 需要再次尝试登录, 异常内容为服务器返回的错误数据
         """
-        url = 'https://mp.weixin.qq.com/cgi-bin/message?t=message/list&token=%s&count=20&day=7' % self.token
-        payload = {
-            't': 'message/list',
-            'token': self.token,
-            'count': 20,
-            'day': 7,
-        }
+        if star:
+            star_param = '&action=star'
+        else:
+            star_param = ''
+        if lastid == 0:
+            lastid = ''
+
+        url = 'https://mp.weixin.qq.com/cgi-bin/message?t=message/list&f=json&lang=zh_CN{star}&count={count}&day={day}&frommsgid={lastid}&offset={offset}&token={token}'.format(
+            star=star_param,
+            count=count,
+            day=day,
+            lastid=lastid,
+            offset=offset,
+            token=self.__token,
+        )
         headers = {
             'x-requested-with': 'XMLHttpRequest',
-            'referer': 'https://mp.weixin.qq.com/cgi-bin/loginpage?t=wxm2-login&lang=zh_CN',
-            'cookie': self.wx_cookies,
+            'referer': 'https://mp.weixin.qq.com/cgi-bin/message?t=message/list&count=20&day=7&token={token}&lang=zh_CN'.format(token=self.__token),
+            'cookie': self.__cookies,
         }
+        r = requests.get(url, headers=headers)
 
-        r = requests.get(url, data=payload, headers=headers)
+        try:
+            message = json.loads(r.text)['msg_items']
+        except (KeyError, ValueError):
+            raise NeedLoginError(r.text)
 
-        c = "".join(r.text.split())
-        s = re.search(r'list:\((.*)\).msg_item', c)
-        if not s:
-            raise NeedLoginError()
-        else:
-            msg_list = s.group(1)
-        return msg_list
+        return message
 
     # def get_news_list(self):
     #     url = 'https://mp.weixin.qq.com/cgi-bin/appmsg?token=%s&lang=zh_CN&type=10&action=list&begin=0&count=10&f=json&random=0.122' % self.token
