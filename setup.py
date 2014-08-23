a252b2c5caedb6c4b371dbaaaab8c02549042195
doraemonext <doraemonext@gmail.com>
2014-08-23 16:49:58 +0800

    Update: version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'wechat-sdk',
-    version = '0.3.8',
+    version = '0.4.0',
     keywords = ('wechat', 'sdk', 'wechat sdk'),
     description = u'微信公众平台Python开发包',
     long_description = open("README.rst").read(),
