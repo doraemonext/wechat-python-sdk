2a265741c02c978a861f9e9c9723b91f7fa98aed
Cole Smith <uniquecolesmith@gmail.com>
2016-01-12 16:02:50 +0800

    fix bug:     Python 3:         ISSUE: when it executes `from wechat_sdk import WechatBasic`, it raise an error `cannot import name 'WechatBasic'`, beacause it has no module named 'StringIO' when it executes `from StringIO import StringIO` in file `wechat_sdk/basic.py` in python3         SOLUTION: add `from io import StringIO` for python3

--- wechat_sdk/basic.py
+++ wechat_sdk/basic.py
@@ -6,7 +6,11 @@ import time
 import json
 import ast
 import cgi
-from StringIO import StringIO
+
+try:
+    from StringIO import StringIO # python 2
+except ImportError:
+    from io import StringIO # python 3
 
 from .messages import MESSAGE_TYPES, UnknownMessage
 from .exceptions import (
