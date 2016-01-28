dbc1e64d6c1854a4533aa098d18de0295f0e40d8
doraemonext <doraemonext@gmail.com>
2016-01-28 20:25:45 +0800

    Add: 在__init__.py中添加WechatConf

--- wechat_sdk/__init__.py
+++ wechat_sdk/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ['WechatBasic', 'WechatExt']
+__all__ = ['WechatConf', 'WechatBasic', 'WechatExt']
 
 try:
+    from wechat_sdk.core.conf import WechatConf
     from wechat_sdk.basic import WechatBasic
     from wechat_sdk.ext import WechatExt
 except ImportError:
